bf6c884fdaa60c961a802264709a2d678478e21a
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-04-05 23:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://medscribe-three.vercel.app/"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
